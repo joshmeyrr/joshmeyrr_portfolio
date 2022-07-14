d355078a2900e2144205ba4e48e12adfa67379e0
Josh <meyer.josh8@gmail.com>
2022-07-14 15:05:57 -0500

    update

--- components/CookieConsent.vue
+++ components/CookieConsent.vue
@@ -1,43 +1,43 @@
 <template>
-	<div
-		v-if="isOpen"
-		class="
-			fixed
-			bottom-0
-			left-0
-			items-center
-			justify-center
-			w-full
-			py-4
-			px-8
-			bg-dark
-			shadow-custom
-			lg:flex
-			bg-primary-light
-			cookie
-		"
-	>
-		<div class="flex items-center justify-center gap-8">
-			<p class="max-w-sm">We use basic cookies to optimize performance.</p>
-			<div class="flex justify-between">
-				<!-- <button
+	<transition>
+		<div
+			v-if="isOpen"
+			class="
+				fixed
+				bottom-0
+				left-0
+				items-center
+				justify-center
+				w-full
+				py-4
+				px-8
+				bg-dark
+				shadow-custom
+				lg:flex
+			"
+		>
+			<div class="flex items-center justify-center gap-8">
+				<p class="max-w-sm">We use basic cookies to optimize performance.</p>
+				<div class="flex justify-between">
+					<!-- <button
 					class="ml-2 bg-yellow text-primary btn button md:ml-0"
 					@click="accept"
 				>
 					Yes
 				</button> -->
-				<button class="ml-8 text-cta" @click="deny">
-					<font-awesome-icon
-						class="w-8 h-auto text-cta"
-						icon="fa-solid fa-rectangle-xmark"
-					/>
-				</button>
+					<button class="ml-8 text-cta" @click="close">
+						<font-awesome-icon
+							class="w-8 h-auto text-cta"
+							icon="fa-solid fa-rectangle-xmark"
+						/>
+					</button>
+				</div>
 			</div>
 		</div>
-	</div>
+	</transition>
 </template>
 <script>
-import { bootstrap } from 'vue-gtag'
+// import { bootstrap } from 'vue-gtag'
 export default {
 	data() {
 		return {
@@ -55,26 +55,36 @@ export default {
 				return localStorage.getItem('GDPR:accepted', true)
 			}
 		},
-		deny() {
+		close() {
 			if (process.browser) {
 				this.isOpen = false
-				localStorage.setItem('GDPR:accepted', false)
+				localStorage.setItem('GDPR:accepted', true)
 			}
 		},
-		accept() {
-			if (process.browser) {
-				bootstrap().then((gtag) => {
-					this.isOpen = false
-					localStorage.setItem('GDPR:accepted', true)
-					location.reload()
-				})
-			}
-		},
+		// accept() {
+		// 	if (process.browser) {
+		// 		bootstrap().then((gtag) => {
+		// 			this.isOpen = false
+		// 			localStorage.setItem('GDPR:accepted', true)
+		// 			location.reload()
+		// 		})
+		// 	}
+		// },
 	},
 }
 </script>
 <style lang="scss">
 .cookie {
 	z-index: 99;
+}
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+	opacity: 1;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
 		<NavBar />
 		<Nuxt class="container-site py-24" />
 		<Footer />
+
 		<CookieConsent />
 	</div>
 </template>

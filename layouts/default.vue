93750fce05ae5bfd5d5effa34d8c3ee0c76ac91f
Josh <meyer.josh8@gmail.com>
2022-07-15 16:32:42 -0500

    fix padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="min-h-screen">
 		<NavBar class="fixed z-50 w-full container-site shadow-custom" />
-		<Nuxt class="pt-48 container-site" />
+		<Nuxt class="pt-44 container-site" />
 		<Footer class="container-site" />
 
 		<CookieConsent />
